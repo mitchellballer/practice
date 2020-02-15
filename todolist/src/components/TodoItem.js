ed204335b3688c3549167684c18c2ecf465ccece
Mitchell Baller <mhb91@case.edu>
2020-02-15 18:16:56 -0500

    Changes to be committed: 	modified:   todolist/src/components/TodoItem.js 	modified:   todolist/src/components/Todos.js

--- todolist/src/components/TodoItem.js
+++ todolist/src/components/TodoItem.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
     render () {
         return (
             <div>
-                <p> Hello World</p>
+                <p> {this.props.todo.title} </p>
             </div>
         )
     }
--- todolist/src/components/Todos.js
+++ todolist/src/components/Todos.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { TodoItem } from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
     render () {
         return this.props.todos.map((todo) => (
-        <TodoItem />
+        <TodoItem key={todo.id} todo={todo}/>
         ));
     }
+}
+
+TodoItem.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
