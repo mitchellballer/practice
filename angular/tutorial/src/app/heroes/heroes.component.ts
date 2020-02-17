064cb7ed7eb70bf4978381fed34d2ad46f18032b
Mitchell Baller <mhb91@case.edu>
2020-02-16 20:02:19 -0500

    adding a list. Angular operations to that list like iterating through list of heros, conditional css if hero list item selected

--- angular/tutorial/src/app/heroes/heroes.component.ts
+++ angular/tutorial/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,17 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
   hero: Hero = {
     id: 1,
     name: "Windstorm",
   };
-  
+
   constructor() { 
 
   }
