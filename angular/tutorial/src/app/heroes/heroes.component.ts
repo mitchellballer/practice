cd7686ede150e9c9ec27f72fa2364da93bc6e238
Mitchell Baller <mhb91@case.edu>
2020-02-18 21:58:43 -0500

    HeroService is now asynchronous

--- angular/tutorial/src/app/heroes/heroes.component.ts
+++ angular/tutorial/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,8 +10,11 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
@@ -20,11 +24,15 @@ export class HeroesComponent implements OnInit {
     name: "Windstorm",
   };
 
-  constructor() { 
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  
+  ngOnInit() {
+    this.getHeroes();
   }
 
-  ngOnInit(): void {
-  }
+
 
 }
