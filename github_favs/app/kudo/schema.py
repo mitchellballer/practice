4b573e858c9b5c85949e720e6500844a9fc39ffc
Mitchell Baller <mhb91@case.edu>
2020-04-15 01:08:15 -0400

    kudos updates

--- github_favs/app/kudo/schema.py
+++ github_favs/app/kudo/schema.py
@@ -1,3 +1,14 @@
 """
 purpose of the schema is to represent kudo data and serve as reference to validate incoming request payload
 """
+
+from marshmallow import Schema, fields
+
+class GithubRepoSchema(Schema):
+    id = fields.Int(required=True)
+    repo_name = fields.Str()
+    full_name = fields.Str()
+    description = fields.Str()
+
+class KudoSchema(GithubRepoSchema):
+    user_id = fields.Email(required=True)
