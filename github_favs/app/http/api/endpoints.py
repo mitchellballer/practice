3c2d4f5b95e5b30de1eb7cb6907250d78e63c214
Mitchell Baller <mhb91@case.edu>
2020-04-20 00:13:12 -0400

    wrapping up for now

---
+++ github_favs/app/http/api/endpoints.py
@@ -0,0 +1,5 @@
+from .middlewares import login_required
+from flask import Flask
+
+#left off https://developer.okta.com/blog/2019/03/25/build-crud-app-with-python-flask-angular
+#can't get import statements to work
