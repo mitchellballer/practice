043d4358e9a2e8c8a7e40c4df02e2cec2833f56a
Mitchell Baller <mhb91@case.edu>
2020-04-20 00:57:17 -0400

    cleanup and angular client side

--- angular-python-flask/src/app/app.component.ts
+++ angular-python-flask/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-page';
+  serverData: JSON;
+  employeeData: JSON;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON;
+      console.log(this.serverData);
+    })
+  }
+
+
+  getAllEmployees() {
+    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
+      this.employeeData = data as JSON;
+      console.log(this.employeeData);
+    })
+  }
 }
