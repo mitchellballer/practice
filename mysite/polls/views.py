62eab1e2cd055c2962c001f8148139a6586930fb
Mitchell Baller <mhb91@case.edu>
2020-05-05 23:25:26 -0400

    index page only shows questions posted in the past. Test that functionality

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -14,8 +15,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        return the last five published questions. 
+        (not including those set to be published in the future
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
